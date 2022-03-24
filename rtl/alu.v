a09698cfc5016d45314a4548a99281ec0975bd41
enjou <enjou1224@outlook.com>
2022-03-24 16:02:06 +0800

    开始动工ALU,大概想好了输入输出接口，等待完成

--- rtl/alu.v
+++ rtl/alu.v
@@ -2,9 +2,27 @@
 /*
     -- ============================================================================
     -- FILE NAME	: alu.v
-    -- DESCRIPTION	: 算术逻辑单元，整合了NextPC生成
+    -- DESCRIPTION	: 算术逻辑单元，整合了部分NextPC生成
     -- ----------------------------------------------------------------------------
     -- Revision	Date		Coding_by	 Comment
     -- 1.0.0	2022/03/24  enjou		 初版 
     -- ============================================================================
 */
+module alu(
+	input  [31:0]	PC,
+	input  [31:0]	Inst,
+
+	// 来自DECODE部分的信号					(Signals from DECODE Part)	
+	input  [11:0]	i_ALUControl_12,
+	input  [31:0]	i_ALUOperand1_32,
+	input  [31:0]	i_ALUOperand2_32,
+
+	// 输出到NextPC部分						(Output to NextPC Part)	
+	output [31:0]	o_JumpBranchAddr_32,
+
+	// 输出到Memory部分						(Output to Memory Part)
+	output [31:0]	o_ALUResult_32
+);
+
+	//--------------------控制信号拆分(Control Signal Splitting)--------------------//
+endmodule
