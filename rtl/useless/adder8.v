2a86416fa2858878d6e2c1b5483a6b407f5977fd
enjou <enjou1224@outlook.com>
2022-05-07 19:57:12 +0800

    整理了一下文件夹

--- rtl/useless/adder8.v
+++
@@ -1,9 +0,0 @@
-module m_adder8(
-		input i_adderOperand1_8,
-		input i_adderOperand2_8,
-		input i_cIn_1,
-		output o_adderSum_8,
-		output o_cOut_1
-);
-	assign {o_cOut_1,o_adderSum_8} 	= i_adderOperand1_8 +i_adderOperand2_8 + i_cIn_1;
-endmodule
